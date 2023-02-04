49032a7fc585788cf82ef97c5ab9b294ae800ef1
Alin <alin@gmail.com>
2023-02-04 18:06:50 +0200

    updated file

--- src/pages/SigninSignup.jsx
+++ src/pages/SigninSignup.jsx
@@ -3,6 +3,7 @@ import Signin from '../components/Signin'
 import Signup from '../components/Signup'
 
 const SigninSignup = () => {
+    // main page
     return (
         <div className="signinup">
             <Signin />
